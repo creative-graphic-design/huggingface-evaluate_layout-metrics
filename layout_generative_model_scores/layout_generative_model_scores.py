6a241fac6efbb63db4401ef40fb145d301a62a39
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-02-12 19:52:17 +0900

    add generative-model-scores (#4)
    * add files
    
    * update

---
+++ layout_generative_model_scores/layout_generative_model_scores.py
@@ -0,0 +1,78 @@
+from typing import Dict, List, Union
+
+import datasets as ds
+import evaluate
+import numpy as np
+import numpy.typing as npt
+from prdc import compute_prdc
+from pytorch_fid.fid_score import calculate_frechet_distance
+
+_DESCRIPTION = """\
+Compute some generative model-based scores.
+"""
+
+_CITATION = """\
+@article{heusel2017gans,
+  title={Gans trained by a two time-scale update rule converge to a local nash equilibrium},
+  author={Heusel, Martin and Ramsauer, Hubert and Unterthiner, Thomas and Nessler, Bernhard and Hochreiter, Sepp},
+  journal={Advances in neural information processing systems},
+  volume={30},
+  year={2017}
+}
+
+@inproceedings{naeem2020reliable,
+  title={Reliable fidelity and diversity metrics for generative models},
+  author={Naeem, Muhammad Ferjad and Oh, Seong Joon and Uh, Youngjung and Choi, Yunjey and Yoo, Jaejun},
+  booktitle={International Conference on Machine Learning},
+  pages={7176--7185},
+  year={2020},
+  organization={PMLR}
+}
+"""
+
+
+class LayoutGenerativeModelScores(evaluate.Metric):
+    def __init__(self, nearest_k: int = 5, **kwargs):
+        super().__init__(**kwargs)
+        self.nearest_k = nearest_k
+
+    def _info(self) -> evaluate.EvaluationModuleInfo:
+        return evaluate.MetricInfo(
+            description=_DESCRIPTION,
+            citation=_CITATION,
+            features=ds.Features(
+                {
+                    "feats_real": ds.Sequence(ds.Sequence(ds.Value("float64"))),
+                    "feats_fake": ds.Sequence(ds.Sequence(ds.Value("float64"))),
+                }
+            ),
+            codebase_urls=[
+                "https://github.com/CyberAgentAILab/layout-dm/blob/main/src/trainer/trainer/helpers/metric.py#L37-L59",
+                "https://github.com/clovaai/generative-evaluation-prdc",
+                "https://github.com/mseitzer/pytorch-fid",
+            ],
+        )
+
+    def _compute(
+        self,
+        *,
+        feats_real: Union[List[List[List[float]]], npt.NDArray[np.float64]],
+        feats_fake: Union[List[List[List[float]]], npt.NDArray[np.float64]],
+    ) -> Dict[str, float]:
+        # shape: (1, N, 256) -> (N, 256)
+        feats_real = np.asarray(feats_real).squeeze()
+        feats_fake = np.asarray(feats_fake).squeeze()
+
+        mu_real = np.mean(feats_real, axis=0)
+        mu_fake = np.mean(feats_fake, axis=0)
+
+        sigma_real = np.cov(feats_real, rowvar=False)
+        sigma_fake = np.cov(feats_fake, rowvar=False)
+
+        results_prdc = compute_prdc(
+            real_features=feats_real, fake_features=feats_fake, nearest_k=self.nearest_k
+        )
+        result_fid = calculate_frechet_distance(
+            mu1=mu_real, sigma1=sigma_real, mu2=mu_fake, sigma2=sigma_fake
+        )
+        return {**results_prdc, "fid": result_fid}
---
+++ tests/layout_generative_model_scores_test.py
@@ -0,0 +1,62 @@
+import math
+import os
+import pathlib
+from typing import Dict
+
+import evaluate
+import numpy as np
+import pytest
+
+
+@pytest.fixture
+def base_dir() -> str:
+    return "layout_generative_model_scores"
+
+
+@pytest.fixture
+def metric_path(base_dir: str) -> str:
+    return os.path.join(base_dir, "layout_generative_model_scores.py")
+
+
+@pytest.fixture
+def test_fixture_dir() -> pathlib.Path:
+    return pathlib.Path(__file__).parents[1] / "test_fixtures"
+
+
+@pytest.fixture
+def feats_real(test_fixture_dir: pathlib.Path):
+    return np.load(test_fixture_dir / "feats_real.npy")
+
+
+@pytest.fixture
+def feats_fake(test_fixture_dir: pathlib.Path):
+    return np.load(test_fixture_dir / "feats_fake.npy")
+
+
+@pytest.fixture
+def expected_scores() -> Dict[str, float]:
+    return {
+        "precision": 0.7633949739212897,
+        "recall": 0.9025604551920341,
+        "density": 0.6361308677098152,
+        "coverage": 0.7889995258416311,
+        "fid": 3.553096292242742,
+    }
+
+
+def test_metric(
+    metric_path: str,
+    feats_real,
+    feats_fake,
+    expected_scores: Dict[str, float],
+):
+    metric = evaluate.load(path=metric_path)
+    metric.add(feats_real=feats_real, feats_fake=feats_fake)
+
+    scores = metric.compute()
+    assert scores is not None
+
+    for k in expected_scores.keys():
+        score = scores[k]
+        expected_score = expected_scores[k]
+        assert math.isclose(score, expected_score, rel_tol=1e-5)
