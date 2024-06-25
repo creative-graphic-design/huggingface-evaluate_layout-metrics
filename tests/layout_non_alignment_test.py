449fa97627537a98032534b6b71e45c8e75e5a42
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-06-25 23:17:39 +0900

    update

--- tests/layout_non_alignment_test.py
+++ tests/layout_non_alignment_test.py
@@ -7,7 +7,7 @@ import torch
 
 
 @pytest.fixture
-def base_dir(metrics_dir: pathlib.Path) -> str:
+def base_dir(metrics_dir: pathlib.Path) -> pathlib.Path:
     return metrics_dir / "layout_non_alignment"
 
 
